7a9c28ac94ddc9888660bb755b0efc0a20871349
Hendrik Voß <hendrik-voss@arcor.de>
2020-02-28 15:38:28 +0100

    Sorting added

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -2,35 +2,68 @@ import React, {Component} from "react";
 import {connect} from "react-redux";
 import {AppState} from "../store";
 import Question from "../service/model/Question";
-import Poll, {PREVIEW} from "./poll/Poll";
+import {Tab, Tabs} from "@material-ui/core";
+import Poll, {PREVIEW, STATISTICS} from "./poll/Poll";
+import User from "../service/model/User";
 
-class Dashboard extends Component<{ questions: Question[] }> {
+class Dashboard extends Component<{ questions: Question[], loggedIn: User }> {
 
-    componentDidMount(): void {
-
-    }
+    state = {
+        tabNumber: 0,
+    };
 
     render(): React.ReactElement<any, string | React.JSXElementConstructor<any>> | string | number | {} | React.ReactNodeArray | React.ReactPortal | boolean | null | undefined {
-
         return (
             <>
-                {this.props.questions.map(question =>
-                        // <Poll key={question.id} questionId={question.id} pollType={TAKEPOLL}/>
-                        <Poll key={question.id} questionId={question.id} pollType={PREVIEW}/>
-                    // <Poll key={question.id} question={question} pollType={STATISTICS}/>
-                )}
+                <Tabs
+                    value={this.state.tabNumber}
+                    onChange={(event, value) => this.setState({...this.state, tabNumber: value})}
+                    indicatorColor="primary"
+                    textColor="primary"
+                    centered
+                    style={{flexGrow: 1,}}
+                >
+                    <Tab label="Unanswered Questions"/>
+                    <Tab label="Answered Questions"/>
+                </Tabs>
+                {this.state.tabNumber === 0 && this.renderUnAnswered()}
+                {this.state.tabNumber === 1 && this.renderAnswered()}
             </>
         );
     }
 
+    private renderUnAnswered = () => this.props.questions
+        .filter(question => !this.answeredByAuthedCharacter(question))
+        .sort(this.byDate)
+        .map(question =>
+            <Poll key={question.id} questionId={question.id} pollType={PREVIEW}/>
+        );
+
+    private renderAnswered = () => this.props.questions
+        .filter(this.answeredByAuthedCharacter)
+        .sort(this.byDate)
+        .map(question =>
+            <Poll key={question.id} questionId={question.id} pollType={STATISTICS}/>
+        );
+
+
+    private answeredByAuthedCharacter = (question: Question): boolean => {
+        const loggedIn: string = this.props.loggedIn.id;
+        return question.optionOne.userVotedFor.includes(loggedIn) || question.optionTwo.userVotedFor.includes(loggedIn);
+    };
+
+    private byDate = (a: Question, b: Question) => {
+        return b.timestamp - a.timestamp
+    };
 
 }
 
 
-function mapStatToProps(state: AppState): { questions: Question[] } {
+function mapStatToProps(state: AppState): { questions: Question[], loggedIn: User } {
     console.log("appState", state);
     return {
         questions: state.questionState.questions,
+        loggedIn: state.choseCharacter.authedUser as User,
     };
 }
 
