4e5d29ebc81330677f5f5c9aa7aaa1678f822280
Hendrik Sebastian Voß <hendrik@familievoss.org>
2020-02-28 23:06:07 +0100

    simple Leaderboard

--- src/components/Leaderboard.tsx
+++ src/components/Leaderboard.tsx
@@ -1,35 +1,46 @@
 import React, {Component} from "react";
 import {connect} from "react-redux";
 import {AppState} from "../store";
-import Question from "../service/model/Question";
-import Poll, {STATISTICS} from "./poll/Poll";
+import User from "../service/model/User";
+import Card from "@material-ui/core/Card";
+import CardHeader from "@material-ui/core/CardHeader";
+import {Avatar, Box, CardContent} from "@material-ui/core";
 
-class Leaderboard extends Component<{ questions: Question[] }> {
-
-    componentDidMount(): void {
-
-    }
-
+class Leaderboard extends Component<{ users: User[] }> {
     render(): React.ReactElement<any, string | React.JSXElementConstructor<any>> | string | number | {} | React.ReactNodeArray | React.ReactPortal | boolean | null | undefined {
-
         return (
             <>
-                {this.props.questions.map(question =>
-                    // <Poll key={question.id} question={question} pollType={TAKEPOLL}/>
-                    // <Poll key={question.id} question={question} pollType={PREVIEW}/>
-                    <Poll key={question.id} questionId={question.id} pollType={STATISTICS}/>
-                )}
+                {this.props.users
+                    .sort((a, b) => (b.questionIDs.length + b.answers.length) - (a.questionIDs.length + a.answers.length))
+                    .map(user =>
+                        <Card key={user.id} variant={"outlined"} style={{maxWidth: 800, minWidth: 320}}>
+                            <CardHeader
+                                avatar={<Avatar alt={user.name} src={user.avatarURL}/>}
+                                title={user.name}
+                            />
+                            <CardContent>
+                                <Box textAlign="left" m={1}>
+                                    Questions asked: {user.questionIDs.length}
+                                </Box>
+                                <Box textAlign="left" m={1}>
+                                    Questions answered: {user.answers.length}
+                                </Box>
+                                <Box textAlign="right" fontSize="h6.fontSize" m={1}>
+                                    Score: {user.questionIDs.length + user.answers.length}
+                                </Box>
+
+                            </CardContent>
+                        </Card>
+                    )}
             </>
         );
     }
-
-
 }
 
 
-function mapStatToProps(state: AppState): { questions: Question[] } {
+function mapStatToProps(state: AppState): { users: User[] } {
     return {
-        questions: state.questionState.questions,
+        users: state.userState.users,
     };
 }
 
