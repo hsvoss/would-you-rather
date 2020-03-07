348d9662345abaaf95ac9edb45ce0da4bc9f5322
Hendrik Sebastian Voß <hendrik@familievoss.org>
2020-03-07 20:50:44 +0100

    404 - Page: added Link to Dashboard

--- src/components/PageFourOFour.tsx
+++ src/components/PageFourOFour.tsx
@@ -1,6 +1,12 @@
 import React from "react";
+import {Link} from "react-router-dom";
+import {Button} from "@material-ui/core";
 
-const PageFourOFour = () => <p>Page not Found</p>;
+const PageFourOFour = () =>
+    <>
+        <p>404 - Page not Found</p>
+        <Link to={'/'}><Button variant={'contained'} color={'primary'}>Go back to Dashboard</Button></Link>
+    </>;
 
 export default PageFourOFour
 
